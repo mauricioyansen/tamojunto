536e6e84f784b4bc67788b6bdb14ca2eb2671615
Mauricio <mauricio_cy@hotmail.com>
2025-05-04 23:45:50 -0300

    fix: typescript errors fix

--- src/app/api/signin/route.ts
+++ src/app/api/signin/route.ts
@@ -1,4 +1,3 @@
-// src/app/api/login/route.ts
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 import bcrypt from "bcryptjs";
@@ -10,16 +9,16 @@ export async function POST(req: Request) {
   try {
     const user = await prisma.user.findUnique({ where: { email } });
 
-    if (!user || !(await bcrypt.compare(password, user.password))) {
+    if (
+      !user ||
+      !user.password ||
+      !(await bcrypt.compare(password, user.password))
+    ) {
       return NextResponse.json(
         { error: "Email ou senha inválidos" },
         { status: 401 }
       );
     }
-
-    // Aqui, você poderia gerar um token ou criar uma sessão
-    // mas como você quer apenas redirecionar, só retornamos o user
-
     return NextResponse.json({ user }, { status: 200 });
   } catch (error) {
     return NextResponse.json(
